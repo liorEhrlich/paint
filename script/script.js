bd549db11a80dd18efb76e9f665ee8955c4debf1
liorEhrlich <37666571+liorEhrlich@users.noreply.github.com>
2018-11-09 12:34:08 +0200

    Added brush size

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 var chosenColor = "black";
 var idCounter = 0;
-var flag=true;
+var flag = true;
+var brushSize = 10;
 
 var colorPicker = function (e) {
     switch (e.target.id) {
@@ -32,59 +33,79 @@ var colorPicker = function (e) {
 }
 
 var drawOnCanvasDot = function (e) {
-    if(flag==true){
-    var x = e.clientX - document.querySelector(".my-canvas").getBoundingClientRect().x;
-    var y = e.clientY - document.querySelector(".my-canvas").getBoundingClientRect().y;
-    var circler = document.createElement("div");
-    var idCircle = circler.setAttribute("id",`circle-${idCounter}`);
-    var classCircle = circler.setAttribute("class","my-circle");
-    document.getElementById("canvas-id").appendChild(circler);
-    var el = document.getElementById(`circle-${idCounter}`);
-    el.style.left = x+"px";
-    el.style.top = y+"px";
-    el.style.position = "absolute"
-    el.style.background = chosenColor;
-    idCounter++;
-    document.querySelector(".my-canvas").addEventListener("mouseup", function(){return false;});
-}}
-
-var stopFunction = function(){
-    flag=false;
+    if (flag == true) {
+        var x = e.clientX - document.querySelector(".my-canvas").getBoundingClientRect().x;
+        var y = e.clientY - document.querySelector(".my-canvas").getBoundingClientRect().y;
+        var circler = document.createElement("div");
+        var idCircle = circler.setAttribute("id", `circle-${idCounter}`);
+        var classCircle = circler.setAttribute("class", "my-circle");
+        document.getElementById("canvas-id").appendChild(circler);
+        var el = document.getElementById(`circle-${idCounter}`);
+        el.style.left = x + "px";
+        el.style.top = y + "px";
+        el.style.width = brushSize + "px";
+        el.style.height = brushSize + "px";
+        el.style.position = "absolute"
+        el.style.background = chosenColor;
+        idCounter++;
+        document.querySelector(".my-canvas").addEventListener("mouseup", function () { return false; });
+    }
 }
 
-var erase = function(){
+var stopFunction = function () {
+    flag = false;
+}
+
+var erase = function () {
     chosenColor = "white";
 }
 
-var save = function(){
+var save = function () {
     var savedProject = document.querySelectorAll("#canvas-id div");
     savedProject = JSON.stringify(savedProject);
-    localStorage.setItem("savedDrawing",savedProject);
+    localStorage.setItem("savedDrawing", savedProject);
 }
 
-var load = function(){
+var load = function () {
     var loadProject = localStorage.getItem("savedDrawing");
     loadProject = JSON.parse(loadProject);
     var parent = document.getElementById("canvas-id");
     var childElements = document.querySelectorAll("#canvas-id div");
-    for(var i = 0;i<childElements;i++){
+    for (var i = 0; i < childElements; i++) {
         var child = document.querySelector("#canvas-id div");
         parent.removeChild(child);
     }
-    for(var j=0;j<loadProject.length;i++){
+    for (var j = 0; j < loadProject.length; i++) {
         parent.appendChild(loadProject[i]);
+    }
+}
+
+var changeBrushSize = function () {
+    switch (brushSize) {
+        case 10:
+            brushSize = 20
+            break;
+        case 20:
+            brushSize = 5
+            break;
+        case 5:
+            brushSize=10
+            break;
     }
 }
 
 document.querySelector(".my-pallete").addEventListener("click", colorPicker);
 
-document.querySelector(".my-canvas").addEventListener("mousedown", function(){
-    flag=true;
-    document.querySelector(".my-canvas").addEventListener("mousemove",drawOnCanvasDot)
+document.querySelector(".my-canvas").addEventListener("mousedown", function () {
+    flag = true;
+    document.querySelector(".my-canvas").addEventListener("mousemove", drawOnCanvasDot)
 });
-document.querySelector(".my-canvas").addEventListener("mouseup",stopFunction);
+document.querySelector(".my-canvas").addEventListener("mouseup", stopFunction);
 
-document.querySelector("#save").addEventListener("click",save);
+document.querySelector("#save").addEventListener("click", save);
 
-document.querySelector("#load").addEventListener("click",load);
+document.querySelector("#load").addEventListener("click", load);
+
+document.querySelector("#brushSize").addEventListener("click", changeBrushSize);
+
 
